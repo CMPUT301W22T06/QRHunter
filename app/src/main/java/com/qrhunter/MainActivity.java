e9c977333efa66f9815c0281d107ed4dc73e839b
Kyle Kernick <kkernick@ualberta.ca>
2022-03-02 20:20:35 -0700

    Added Geolocation support to the Collectable.

--- app/src/main/java/com/qrhunter/MainActivity.java
+++ app/src/main/java/com/qrhunter/MainActivity.java
@@ -3,14 +3,18 @@ package com.qrhunter;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -31,47 +35,76 @@ public class MainActivity extends AppCompatActivity {
     DecoratedBarcodeView scanner;
     Collectable scanned;
 
+
+    /**
+     * Stores the location within Collectable, as long as permissions are granted.
+     *
+     * This function checks for permission and handles it to save location data within the scanned
+     * Collectable, calling upload once the data has been stored. Because storeLocation uploads
+     * the Collectable, location must be the LAST piece of data given to the Collectable before
+     * its uploaded.
+     */
+    private void storeLocation() {
+
+        // If permission is not granted, we need to ask and then leave
+        // onRequestPermissionsResult will handle uploading after the user select a option.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            return;
+        }
+
+        // If permission is already granted, append the location information and upload.
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null)
+                scanned.setLocation(new Pair<>(locationGPS.getLatitude(), locationGPS.getLongitude()));
+            else
+                Toast.makeText(getApplicationContext(), "Unable to find location.", Toast.LENGTH_SHORT).show();
+        upload();
+    }
+
+    /**
+     * Uploads the Collectable, and resumes the scanner.
+     *
+     * This function helps for avoiding async issues with location permission when creating the
+     * Collectable.
+     */
+    private void upload() {
+        // TODO Upload the Collectable to the database.
+        scanner.resume();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-         * We need access to the camera if we want to scan barcodes. If the user declines to
-         * accept the permission, request again and send a Toast to let them know that they need
-         * to enable the permission. This will also toast when first requesting the permission,
-         * which can let the user know why the permission is needed.
-         */
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(getApplicationContext(), "This program requires access to the camera to scan QR Codes!", Toast.LENGTH_SHORT).show();
+        // Request Camera Permission (Needed for the scanner)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
-        }
 
-        /*
-         * The results from the camera activity. We use this to grab the BitMap and store it into
-         * the Collectable.
-         */
+        // Register for the callback when the camera activity returns (It has to be here.)
         ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                camera_result -> {
-
-                    // Results other than OK we can just ignore, the photo already defaults to null.
-                    if (camera_result.getResultCode() == Activity.RESULT_OK) {
-                        Intent data = camera_result.getData();
-                        assert(data != null);
-                        scanned.setPhoto((Bitmap)data.getExtras().get("data"));
-                    }
+            new ActivityResultContracts.StartActivityForResult(),
+            camera_result -> {
+                // Results other than OK we can just ignore, the photo already defaults to null.
+                if (camera_result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = camera_result.getData();
+                    assert(data != null);
+                    scanned.setPhoto((Bitmap)data.getExtras().get("data"));
                 }
+            }
         );
 
         // Grab the scanner within the activity.
         scanner = findViewById(R.id.main_scanner);
         scanner.decodeContinuous(new BarcodeCallback() {
-            @Override
-            public void barcodeResult(BarcodeResult result) {
 
-                // Create/Overwrite the collectable.
+            // When we successfully scan a code.
+            @Override public void barcodeResult(BarcodeResult result) {
                 scanned = new Collectable();
+                scanned.setId(result.getText());
+                // TODO Store the score of the code.
 
                 // Pause the scanner so it doesn't make an infinite amount of popups.
                 scanner.pause();
@@ -80,13 +113,17 @@ public class MainActivity extends AppCompatActivity {
                 LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
                 View context_view = layoutInflater.inflate(R.layout.context_scanned, null);
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                alertDialogBuilder.setOnCancelListener(
-                        // Upload the Collectable to the database.
-                        dialogInterface -> scanner.resume()
-                );
 
-                alertDialogBuilder.setView(context_view);
-                AlertDialog alert = alertDialogBuilder.create();
+                // When the dialog is canceled (IE clicked off of it), save our information.
+                alertDialogBuilder.setOnCancelListener(dialog -> {
+
+                    // Geolocation is optional, so make sure the checkbox was explicitly checked before adding it.
+                    if (((CheckBox)context_view.findViewById(R.id.context_scanned_save_location)).isChecked())
+                        storeLocation();
+                    else {
+                        upload();
+                    }
+                });
 
                 // When we hit add picture, spawn a camera instance and get the BitMap taken.
                 context_view.findViewById(R.id.context_scanned_add_picture).setOnClickListener(v -> {
@@ -94,21 +131,38 @@ public class MainActivity extends AppCompatActivity {
                     cameraResult.launch(cameraIntent);
                 });
 
-                // Set the ID (and add the score later on)
-                scanned.setId(result.getText());
-                alert.show();
+                // Create and show the dialog.
+                alertDialogBuilder.setView(context_view);
+                alertDialogBuilder.create().show();
             }
             @Override public void possibleResultPoints(List<ResultPoint> resultPoints) {}
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (!scanner.isActivated())
-            scanner.resume();
-    }
+    @Override protected void onResume() {super.onResume(); if (!scanner.isActivated()) scanner.resume();}
+
+    @Override protected void onPause() {super.onPause(); scanner.pause();}
 
     @Override
-    protected void onPause() {super.onPause(); scanner.pause();}
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int x = 0; x < permissions.length; ++x) {
+
+            // Camera isn't a big deal, just toast them to let them know it's kinda important.
+            if (permissions[x].equals(Manifest.permission.CAMERA) && requestCode == 0 && grantResults[x] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "The camera is needed to scan QR codes!", Toast.LENGTH_SHORT).show();
+            }
+
+            // If the user grants location, we should add that location to the Collectable.
+            // ! NOTE: This assumes that the only time this permission is checked is when the checkbox is selected in the popup!
+            else if (permissions[x].equals(Manifest.permission.ACCESS_COARSE_LOCATION) && requestCode == 1) {
+                if (grantResults[x] == PackageManager.PERMISSION_GRANTED) storeLocation();
+                else {
+                    Toast.makeText(getApplicationContext(), "Location permission is required to save location!", Toast.LENGTH_SHORT).show();
+                    upload();
+                }
+            }
+        }
+
+    }
 }
