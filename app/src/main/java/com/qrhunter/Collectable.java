0e9be6cc68e74957a74c8593acd944d68c82dd14
Kyle Kernick <kkernick@ualberta.ca>
2022-03-02 20:20:35 -0700

    Added Geolocation support to the Collectable.

--- app/src/main/java/com/qrhunter/Collectable.java
+++ app/src/main/java/com/qrhunter/Collectable.java
@@ -1,16 +1,20 @@
 package com.qrhunter;
 
 import android.graphics.Bitmap;
+import android.util.Pair;
 import android.widget.ImageView;
 
 public class Collectable {
     private String id;
     private int score;
     private Bitmap photo;
+    private Pair<Double, Double> location = new Pair<>(0.0, 0.0);      // lat, long
+
 
     public int getScore() {return score;}
     public String getId() {return id;}
     public Bitmap getPhoto() {return photo;}
+    public Pair<Double, Double> getLocation() {return location;}
 
     public void setScore(int new_score) {
         assert(new_score >= 0);
@@ -22,6 +26,8 @@ public class Collectable {
     public void setPhoto(Bitmap new_photo) {
         photo = new_photo;
     }
+    public void setLocation(Pair<Double, Double> new_location) {location = new_location;}
+
     public void viewPhoto(ImageView view) {
         view.setImageBitmap(photo);
     }
